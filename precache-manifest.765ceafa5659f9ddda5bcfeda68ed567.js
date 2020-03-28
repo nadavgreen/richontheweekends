f907956906130e72d74ede2d4919cd987bee5273
Nadav Green <richontheweekends@gmail.com>
2020-03-28 18:21:43 -0400

    Updates

---
+++ precache-manifest.765ceafa5659f9ddda5bcfeda68ed567.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e515af42bdb4da5a57da5810225ae2f6",
+    "url": "/richontheweekends/index.html"
+  },
+  {
+    "revision": "dced59766e47495f2c8b",
+    "url": "/richontheweekends/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2249f46235277170b672",
+    "url": "/richontheweekends/static/js/2.7453b9be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/richontheweekends/static/js/2.7453b9be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dced59766e47495f2c8b",
+    "url": "/richontheweekends/static/js/main.031ce718.chunk.js"
+  },
+  {
+    "revision": "febc907fed265ecf40f1",
+    "url": "/richontheweekends/static/js/runtime-main.d53a96ec.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/richontheweekends/static/media/logo.5d5d9eef.svg"
+  }
+]);
